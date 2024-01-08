71ba2b51ff1e8b2cfa83c32561928ebb95b93984
yzzze333 <8231117@edu.miet.ru>
2024-01-08 15:43:16 +0700

    Fix/3rd task

--- dz-3.js
+++ dz-3.js
@@ -3,14 +3,10 @@ const date = '2009-01-08';
 function checkAge(bornTime) {
     const bornDate = new Date(bornTime);
     const nowDate = new Date();
-    if (((nowDate.getFullYear()) - bornDate.getFullYear()) > 14) {
-        if (bornDate.getMonth() <= nowDate.getMonth()) {
-            if (bornDate.getDate() <= nowDate.getDate()){
-                return true
-            };
-        };
-    };
-    return false
+    if (((nowDate.getFullYear()) - bornDate.getFullYear()) < 14) return false;
+    if (bornDate.getMonth() > nowDate.getMonth()) return false;
+    if (bornDate.getDate() > nowDate.getDate()) return false;
+    return true;
 };
 
-console.log(checkAge(date))
+console.log(checkAge(date));
