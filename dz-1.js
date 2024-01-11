376eb3ca2767c1e5f12c1f9d748f5f7007f8387a
yzzze333 <8231117@edu.miet.ru>
2024-01-11 16:29:01 +0700

    Fix/changed variable name

--- dz-1.js
+++ dz-1.js
@@ -6,10 +6,10 @@ const data = [
 
 
 const ids = new Set(data.map(x => x.id));
-const o = [];
+const uniqueData = [];
 for (const id of ids) {
     const name = data.find(x => x.id === id).name;
-    o.push({id,name});
+    uniqueData.push({id,name});
 };
 
-console.log(o)
+console.log(uniqueData)
